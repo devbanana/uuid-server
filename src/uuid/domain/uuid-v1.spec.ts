de3ba82ee05d12977a8b3b14150f6728c4610548
Brandon Olivares <programmer2188@gmail.com>
2021-10-12 01:55:30 -0400

    Add tests to check for error conditions

--- src/uuid/domain/uuid-v1.spec.ts
+++ src/uuid/domain/uuid-v1.spec.ts
@@ -16,6 +16,24 @@ describe('UuidV1', () => {
     expect(uuidV1.asString()).toBe(uuid);
   });
 
+  it('should require a validly formatted UUID', () => {
+    expect(() => UuidV1.fromUuid('foo')).toThrowError(
+      /foo is not a valid UUID/,
+    );
+
+    // UUID is missing hyphen after first field
+    expect(() =>
+      UuidV1.fromUuid('65b485402b20-11ec-946a-37aba894513f'),
+    ).toThrowError(/is not a valid UUID/);
+  });
+
+  it('should require a V1 UUID', () => {
+    // Is V4 UUID
+    expect(() =>
+      UuidV1.fromUuid('816fd5e3-dea6-4f79-b2be-41dea6d4f503'),
+    ).toThrowError(/is not a V1 UUID/);
+  });
+
   it("should extract the UUID's time", () => {
     expect(uuidV1.getTime()).toStrictEqual(
       new Date('2021-10-11T17:15:18.426Z'),

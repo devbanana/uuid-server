5786243f22947aa0812febd5b85a273b3054fccb
Brandon Olivares <programmer2188@gmail.com>
2021-10-15 07:50:12 -0400

    Remove custom minDate implementation
     Use isBefore from validator instead.

--- src/uuid/application/min-date-string.validator.ts
+++ src/uuid/application/min-date-string.validator.ts
@@ -4,10 +4,8 @@ import {
   ValidationArguments,
   ValidationOptions,
 } from 'class-validator';
-
-function minDateString(date: string, minDate: string): boolean {
-  return new Date(date) >= new Date(minDate);
-}
+import validator from 'validator';
+import isBefore = validator.isBefore;
 
 export default function MinDateString(
   minDate: string,
@@ -21,7 +19,7 @@ export default function MinDateString(
         value: string,
         validationArguments: ValidationArguments,
       ): boolean {
-        return minDateString(value, validationArguments.constraints[0]);
+        return !isBefore(value, validationArguments.constraints[0]);
       },
       defaultMessage: buildMessage(eachPrefix => {
         return `${eachPrefix}$property cannot be before $constraint1`;

dc3c98e14f1afb4d67bf655531096840992abaed
Brandon Olivares <programmer2188@gmail.com>
2021-10-16 22:09:19 -0400

    Match against regex error instead of entire string

--- src/uuid/domain/uuid-time.spec.ts
+++ src/uuid/domain/uuid-time.spec.ts
@@ -18,7 +18,7 @@ describe('UuidTime', () => {
 
   it('cannot be prior to 1582-10-15', () => {
     expect(() => UuidTime.fromString('1582-10-14T23:59:59Z')).toThrowError(
-      /time cannot be before 1582-10-15 at 00:00:00 UTC/,
+      /time cannot be before/,
     );
   });
 
@@ -30,7 +30,7 @@ describe('UuidTime', () => {
 
   it('cannot be later than 5236-03-31 at 21:21:00.683 UTC', () => {
     expect(() => UuidTime.fromString('5236-03-31T21:21:00.684Z')).toThrowError(
-      /time cannot be after 5236-03-31 at 21:21:00.683 UTC/,
+      /time cannot be after/,
     );
   });
 

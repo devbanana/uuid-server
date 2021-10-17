c506aadbef0207ccf88083c5c060a71f0aa56c71
Brandon Olivares <programmer2188@gmail.com>
2021-10-17 03:16:46 -0400

    Add fromMilliseconds() method to UuidTime

--- src/uuid/domain/uuid-time.spec.ts
+++ src/uuid/domain/uuid-time.spec.ts
@@ -1,4 +1,4 @@
-import { gregorianStart, UuidTime } from './uuid-time';
+import { gregorianStart, maxMs, UuidTime } from './uuid-time';
 
 describe('UuidTime', () => {
   it('should be defined', () => {
@@ -16,27 +16,64 @@ describe('UuidTime', () => {
     );
   });
 
-  it('cannot be prior to 1582-10-15', () => {
-    expect(() => UuidTime.fromString('1582-10-14T23:59:59Z')).toThrowError(
-      /time cannot be before/,
+  it('can accept milliseconds', () => {
+    expect(UuidTime.fromMilliseconds(1_634_451_225_740).asMilliseconds()).toBe(
+      1_634_451_225_740,
     );
   });
 
-  it('can be exactly 1582-10-15 at midnight UTC', () => {
-    expect(UuidTime.fromString('1582-10-15T00:00:00Z').asMilliseconds()).toBe(
-      gregorianStart,
-    );
-  });
+  describe.each`
+    type              | minDate                   | maxDate
+    ${'date string'}  | ${'1582-10-15T00:00:00Z'} | ${'5236-03-31T21:21:00.683Z'}
+    ${'milliseconds'} | ${gregorianStart}         | ${maxMs}
+  `(
+    'using $type',
+    ({
+      minDate,
+      maxDate,
+    }: {
+      minDate: string | number;
+      maxDate: string | number;
+    }) => {
+      it('cannot be prior to 1582-10-15 at midnight UTC', () => {
+        expect(() => {
+          if (typeof minDate === 'string') {
+            const date = new Date(minDate).getTime() - 1;
 
-  it('cannot be later than 5236-03-31 at 21:21:00.683 UTC', () => {
-    expect(() => UuidTime.fromString('5236-03-31T21:21:00.684Z')).toThrowError(
-      /time cannot be after/,
-    );
-  });
+            return UuidTime.fromString(new Date(date).toISOString());
+          }
 
-  it('can be exactly 5236-03-31 at 21:21:00.683 UTC', () => {
-    expect(() =>
-      UuidTime.fromString('5236-03-31T21:21:00.683Z').asMilliseconds(),
-    ).not.toThrowError();
-  });
+          return UuidTime.fromMilliseconds(minDate - 1);
+        }).toThrowError(/time cannot be before/);
+      });
+
+      it('can be exactly 1582-10-15 at midnight UTC', () => {
+        expect(() =>
+          typeof minDate === 'string'
+            ? UuidTime.fromString(minDate)
+            : UuidTime.fromMilliseconds(minDate),
+        ).not.toThrowError();
+      });
+
+      it('cannot be later than 5236-03-31 at 21:21:00.683 UTC', () => {
+        expect(() => {
+          if (typeof maxDate === 'string') {
+            const date = new Date(maxDate).getTime() + 1;
+
+            return UuidTime.fromString(new Date(date).toISOString());
+          }
+
+          return UuidTime.fromMilliseconds(maxDate + 1);
+        }).toThrowError(/time cannot be after/);
+      });
+
+      it('can be exactly 5236-03-31 at 21:21:00.683 UTC', () => {
+        expect(() =>
+          typeof maxDate === 'string'
+            ? UuidTime.fromString(maxDate)
+            : UuidTime.fromMilliseconds(maxDate),
+        ).not.toThrowError();
+      });
+    },
+  );
 });
--- src/uuid/domain/uuid-time.ts
+++ src/uuid/domain/uuid-time.ts
@@ -7,23 +7,26 @@ export const gregorianStart = -12_219_292_800_000;
 export const maxMs = 103_072_857_660_683;
 
 export class UuidTime {
-  private constructor(private time: Date) {}
-
-  static fromString(time: string): UuidTime {
-    const date = new Date(time);
-    if (isNaN(date.getTime())) {
+  private constructor(private time: Date) {
+    if (isNaN(time.getTime())) {
       throw new Error('time must be a valid date string');
     }
 
-    if (date.getTime() < gregorianStart) {
+    if (time.getTime() < gregorianStart) {
       throw new Error('time cannot be before 1582-10-15 at 00:00:00 UTC');
     }
 
-    if (date.getTime() > maxMs) {
+    if (time.getTime() > maxMs) {
       throw new Error('time cannot be after 5236-03-31 at 21:21:00.683 UTC');
     }
+  }
 
-    return new UuidTime(date);
+  static fromString(time: string): UuidTime {
+    return new UuidTime(new Date(time));
+  }
+
+  static fromMilliseconds(ms: number): UuidTime {
+    return new UuidTime(new Date(ms));
   }
 
   asMilliseconds(): number {

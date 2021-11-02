3d603e746a632a212bb9d17f748086b2afde8be0
Brandon Olivares <programmer2188@gmail.com>
2021-11-02 16:33:18 -0400

    Override schema to give different example for V3

--- src/uuid/application/generate-uuid.view-model.ts
+++ src/uuid/application/generate-uuid.view-model.ts
@@ -1,9 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class GenerateUuidViewModel {
   /**
    * The generated UUID, provided in the chosen format.
    *
    * @example 716764e0-3884-11ec-9c7c-0bb5416f760e
    */
+  @ApiProperty({
+    format: 'uuid',
+  })
   public readonly uuid: string;
 
   constructor(uuid: string) {
--- src/uuid/infrastructure/uuid-v3.controller.ts
+++ src/uuid/infrastructure/uuid-v3.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
 import { CommandBus } from '@nestjs/cqrs';
 import { GenerateUuidV3Command } from '../application/v3/generate-uuid-v3.command';
 import { GenerateUuidViewModel } from '../application/generate-uuid.view-model';
@@ -13,6 +13,18 @@ export class UuidV3Controller {
    * Generate a V3 UUID.
    */
   @Get('generate')
+  @ApiOkResponse({
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(GenerateUuidViewModel) },
+        {
+          properties: {
+            uuid: { example: 'd4970169-f9a4-31c9-a11b-08609bb119c2' },
+          },
+        },
+      ],
+    },
+  })
   async generate(
     @Query() command: GenerateUuidV3Command,
   ): Promise<GenerateUuidViewModel> {

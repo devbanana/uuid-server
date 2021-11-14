09e51b3e57e56bef290cc1132aa342c3d6deccfa
Brandon Olivares <programmer2188@gmail.com>
2021-11-13 21:06:08 -0500

    Format random ndoe as MAC address

--- utils/random-node-id.js
+++ utils/random-node-id.js
@@ -3,4 +3,12 @@
 const buffer = require('crypto').randomBytes(6);
 buffer[0] |= 0x01;
 
-console.log(buffer.toString('hex'));
+console.log(
+  '%s:%s:%s:%s:%s:%s',
+  buffer.toString('hex', 0, 1),
+  buffer.toString('hex', 1, 2),
+  buffer.toString('hex', 2, 3),
+  buffer.toString('hex', 3, 4),
+  buffer.toString('hex', 4, 5),
+  buffer.toString('hex', 5, 6),
+);

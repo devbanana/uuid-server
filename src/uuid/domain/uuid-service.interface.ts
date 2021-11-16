8f4c4d2243805b18da9424ffeafe86bcdbe7af6d
Brandon Olivares <programmer2188@gmail.com>
2021-11-16 00:11:44 -0500

    Remove  UuidServiceInterface

--- src/uuid/domain/uuid-service.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UuidV1 } from './time-based/uuid-v1';
-import { UuidTime } from './time-based/uuid-time';
-import { ClockSequence } from './time-based/clock-sequence';
-import { Node } from './time-based/node';
-
-export interface UuidServiceInterface {
-  generateV1(time?: UuidTime, clockSeq?: ClockSequence, node?: Node): UuidV1;
-}

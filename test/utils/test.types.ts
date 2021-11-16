02be151c1e38271f0e37fff95c7c989765aeb866
Brandon Olivares <programmer2188@gmail.com>
2021-11-16 00:10:23 -0500

    Remove unused type

--- test/utils/test.types.ts
+++ test/utils/test.types.ts
@@ -1,14 +1,9 @@
-import { UuidServiceInterface } from '../../src/uuid/domain/uuid-service.interface';
 import { UuidFormats } from '../../src/uuid/domain/uuid-formats';
 
 export type UuidMethod<T extends UuidFormats> = `as${Capitalize<T>}`;
 export type UuidMock = {
   [P in UuidFormats as UuidMethod<P>]: jest.Mock<string>;
 };
-
-export type UuidServiceMock = Partial<
-  Record<keyof UuidServiceInterface, jest.Mock<UuidMock>>
->;
 
 export type UuidFormatMap = Record<UuidFormats, string>;
 
